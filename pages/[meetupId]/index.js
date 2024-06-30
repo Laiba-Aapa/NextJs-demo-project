5252cf38e9b35276c513f646196892e2350d6810
Laiba Asif <95169713+Laiba-Aapa@users.noreply.github.com>
2024-06-30 18:34:06 +0500

    Update index.js
    Head wad added to meetupId index.js page

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,6 +1,8 @@
-import React,{Fragment} from 'react'
+import React from 'react'
 import { MongoClient, ObjectId } from 'mongodb';
 import MeetupDetail from '@/components/meetups/MeetupDetail'
+import { Fragment } from 'react';
+import Head from 'next/head';
 function MeetupDetails(props) {
     return (
         <Fragment>
